0f85e39a11354e1cfb0b9e2b28c2be8010f20ffd
gustavogomides <gustavo.gomides7@gmail.com>
2017-06-06 20:42:17 -0300

    merge com admin-area

--- models/DatabaseConnection.php
+++ models/DatabaseConnection.php
@@ -5,7 +5,7 @@ class DatabaseConnection{
     private $host = "localhost";
     private $db_name = "sandvigbookstore";
     private $username = "root";
-    private $password = "admin";
+    private $password = "";
     public $conn;
  
     // get the database connection
