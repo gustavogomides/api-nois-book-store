59db9cba7e68af974c8ea19ec79304da89ecaa4b
gustavogomides <gustavo.gomides7@gmail.com>
2017-06-09 08:37:23 -0300

    removendo teste.php

--- DAO/teste.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$body = "body";
-	$headers = "MIME-Version: 1.0" . "\r\n";
-	$headers .="Content-type:text/html;charset=UTF-8" . "\r\n";
-	$headers .="From: ianbrooks77@comcast.net" . "\r\n";
-	mail("gustavo.gomides7@hotmail.com", "Your Recent Order From Ian's Coding Books Emporium!" ,$body, $headers);
-?>
